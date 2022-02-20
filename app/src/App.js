6b048145f7c1e745edcf93af63a4ebdc4e1819dc
Harsh Patil <harshnilkanth2015@gmail.com>
2022-02-19 19:31:10 -0700

    v1.0

---
+++ app/src/App.js
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import {Button, Card, ProgressBar} from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import GoalSetting from './GoalSetting';
+
+
+const DAYS_IN_A_MONTH = 31;
+
+function InputToday(props) {
+  function handleNo() {
+    props.decrement();
+  }
+
+  function handleYes() {
+    props.increment();
+  }
+
+  return(
+    <div>
+      <Card className="today">
+        Did you complete your goal today?
+        <Button onClick={handleNo} className="today-button" variant="danger">No.</Button>
+        <Button onClick={handleYes} variant="success">Yes!</Button>
+      </Card>
+    </div>
+  )
+}
+
+function App() {
+  const [startAmount, setStartAmount] = useState(0.00);
+  const [amount, setAmount] = useState(startAmount);
+  const [goal, setGoal] = useState('');
+  const [days, setDays] = useState(0);
+  const [noDays, setNoDays] = useState(0);
+
+
+  function checkDays(TotalDays) {
+    if(TotalDays >= DAYS_IN_A_MONTH) {
+      setDays(0);
+      setNoDays(0);
+    }
+  }
+
+  function handleInvest(e) {
+    let money = prompt("Enter the amount you would like to meVest");
+    setAmount(amount + parseFloat(money));
+    setStartAmount(amount + parseFloat(money));
+  }
+
+  function incrementYesDays() {
+    
+    setDays(days + 1);
+    checkDays(days + noDays);
+  }
+
+  function incrementNoDays() {
+    
+    if((amount - (startAmount/DAYS_IN_A_MONTH))<0) {
+      setAmount(0);
+    }
+    else {
+      setAmount(amount - (startAmount/DAYS_IN_A_MONTH));
+    }
+    setNoDays(noDays + 1);
+    checkDays(days + noDays);
+  }
+
+  return (
+    <div className="App">
+      <h1>meVest</h1>
+      <investmentModal/>
+      <Card className='goal-card'>
+        My goal is {goal}
+      </Card>
+      <GoalSetting changeGoal={setGoal} />
+      <Card className='money-card'>
+      Balance : ${amount.toFixed(2)}
+      <Button onClick={handleInvest} className='invest-button' variant='success'>meVest</Button>
+      </Card>
+      <InputToday increment={incrementYesDays} decrement={incrementNoDays} />
+      <div className='progress-div'>
+        <h3 className='progress-title'>Progress {days + noDays} Days</h3>
+        <ProgressBar>
+          <ProgressBar variant="success" striped animated now={(days/DAYS_IN_A_MONTH)*100} key={1}/>
+          <ProgressBar variant="danger" striped animated now={(noDays/DAYS_IN_A_MONTH)*100} key={2}/>
+        </ProgressBar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
